ef3b421728a5012e50cd891035471b44221e9620
dhruvit2003 <pateldhruvit2003@gmail.com>
2025-01-06 01:41:25 +0530

    added deleted functinality and authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
@@ -16,6 +17,12 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+ 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  const { signOut } = useAuthenticator();
 
   return (
     <main>
@@ -23,7 +30,7 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -33,6 +40,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
